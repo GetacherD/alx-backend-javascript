68836836c620be681c24331f51e6f3af62a08d8c
GetacherD <getacher.d@gmail.com>
2022-12-20 23:38:42 +0300

    fix code style

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,11 +1,13 @@
 export default function iterateThroughObject(reportWithIterator) {
   let str = '';
-  for (const val in reportWithIterator) {
-    if (val === 0) {
-      str += reportWithIterator[val];
+  let count = 0;
+  for (const val of reportWithIterator) {
+    if (count === 0) {
+      str += val;
     } else {
-      str += ` | ${reportWithIterator[val]}`;
+      str += ` | ${val}`;
     }
+    count += 1;
   }
   return str;
 }
