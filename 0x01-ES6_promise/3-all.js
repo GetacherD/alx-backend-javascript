c9de4196b184ed631d2a40716c97301bac734def
GetacherD <getacher.d@gmail.com>
2022-12-21 11:35:01 +0300

    test

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,5 +4,5 @@ export default function handleProfileSignup() {
   const res = Promise.all([uploadPhoto(), createUser()]);
   res.then((val) => {
     console.log(val[0].body, val[1].firstName, val[1].lastName);
-  });
+  }).catch(() => new Error('Signup system offline'));
 }
