9b1c65403661a9d205f083597e1d18bbf739dc2b
GetacherD <getacher.d@gmail.com>
2022-12-21 11:00:40 +0300

    promise test

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,6 +2,9 @@ function handleResponseFromAPI(promise) {
   promise.then(() => {
     console.log('Got a response from the API');
     return { status: 200, body: 'success' };
-  }).catch(() => new Error(""));
+  }).catch(() => {
+    console.log('Got a response from the API');
+    return new Error();
+  });
 }
 export default handleResponseFromAPI;
