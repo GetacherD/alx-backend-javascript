edf80c4ab96818d9392839d67ceaf468ac0fa71f
GetacherD <getacher.d@gmail.com>
2022-12-21 15:15:20 +0300

    first

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -6,15 +6,11 @@ export default async function asyncUploadUser() {
     obj.user = data;
   }).catch(() => {
     obj.user = null;
-    obj.photo = null;
-    return Promise.resolve(obj);
   });
   await uploadPhoto().then((data) => {
     obj.photo = data;
   }).catch(() => {
     obj.photo = null;
-    obj.user = null;
-    return Promise.resolve(obj);
   });
   return Promise.resolve(obj);
 }
