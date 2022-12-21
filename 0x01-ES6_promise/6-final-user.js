dd86857b9f9d1be6b9fe99fdb284212fd17c05da
GetacherD <getacher.d@gmail.com>
2022-12-21 14:06:43 +0300

    first

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,8 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const usr = await signUpUser(firstName, lastName).then((data) => ({ status: 'fulfilled', value: data }));
+  const photo = await uploadPhoto(fileName).catch((err) => ({ status: 'rejected', value: err.toString() }));
+  return Promise.resolve([usr, photo]);
+}
