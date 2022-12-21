c65336b2be911c63636b78d80274545c78124f9a
GetacherD <getacher.d@gmail.com>
2022-12-21 10:53:54 +0300

    promise examples

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,6 +2,6 @@ function handleResponseFromAPI(promise) {
   promise.then(() => {
     console.log('Got a response from the API');
     return { status: 200, body: 'success' };
-  }).catch(() => new Error());
+  }).catch(() => new Error(""));
 }
 export default handleResponseFromAPI;
