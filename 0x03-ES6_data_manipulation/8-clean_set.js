1b62487a216e15a7fbc75064b635b1c0cd9cca84
GetacherD <getacher.d@gmail.com>
2023-01-02 20:02:18 +0300

    first

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,10 @@
 export default function cleanSet(st, str) {
   const res = [];
+  if (!str) {
+    return '';
+  }
   st.forEach((item) => {
-    if (String(item).startsWith(str) && str) {
+    if (String(item).startsWith(str)) {
       res.push(String(item).slice(str.length));
     }
   });
