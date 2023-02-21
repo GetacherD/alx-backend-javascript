7866386dd521585721e452635bec63b78fcfbe1b
GetacherD <getacher.d@gmail.com>
2023-02-21 18:23:24 +0300

    use process events

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,10 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf-8');
+console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data.toString()}`);
+  process.stdin.resume();
+  process.stdout.write(`Your name is: ${data}`);
   process.exit(0);
 });
-
 process.on('exit', () => {
   console.log('This important software is now closing');
 });
