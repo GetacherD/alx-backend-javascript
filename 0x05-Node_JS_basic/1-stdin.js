8fd9a3b713020fadd0ef29366e851134995bdfbb
GetacherD <getacher.d@gmail.com>
2023-02-21 17:59:13 +0300

    use process

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,9 @@
-const readline = require('readline');
-
-const rl = readline.createInterface(
-  {
-    input: process.stdin,
-    output: process.stdout,
-  },
-);
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-  rl.close();
-});
-rl.on('close', () => {
-  console.log('This important software is now closing');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('data', (data) => {
+  process.stdout.write(data.toString());
   process.exit(0);
+});
+
+process.on('exit', () => {
+  console.log('This important software is now closing');
 });
