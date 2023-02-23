1249f5cb293913e399204c120b83f70c7ce7f4ed
GetacherD <getacher.d@gmail.com>
2023-02-23 12:03:29 +0300

    test case

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -2,8 +2,16 @@ const sinon = require("sinon")
 const Utils = require("./utils")
 const assert = require("assert")
 describe("Spy", function () {
-        it("spy", function () {
-                const sendPaymentRequestToApi = sinon.spy(Utils, "calculateNumber");
+        let sendPaymentRequestToApi = sinon.spy(Utils, "calculateNumber");
+        it("assert value", function () {
+
+                sendPaymentRequestToApi.returnValues = [300]
+                const res = Utils.calculateNumber("SUM", 4, 6)
+                assert.equal(res, 10);
+
+        })
+        it("assert called", function () {
+
                 sendPaymentRequestToApi.returnValues = [300]
                 const res = Utils.calculateNumber("SUM", 4, 6)
                 assert.equal(res, 10);
