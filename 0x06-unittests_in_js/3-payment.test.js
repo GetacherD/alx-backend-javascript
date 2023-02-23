d44023d24f7cecae58f65e87c81f9558997ec0ff
GetacherD <getacher.d@gmail.com>
2023-02-23 12:12:01 +0300

    assert called

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -2,20 +2,29 @@ const sinon = require("sinon")
 const Utils = require("./utils")
 const assert = require("assert")
 describe("Spy", function () {
-        let sendPaymentRequestToApi = sinon.spy(Utils, "calculateNumber");
+        const sendPaymentRequestToApi = sinon.createSandbox();
+
+        beforeEach(function () {
+                sendPaymentRequestToApi.spy(Utils, "calculateNumber");
+        });
+
+        afterEach(function () {
+                sendPaymentRequestToApi.restore();
+        });
+
         it("assert value", function () {
 
-                sendPaymentRequestToApi.returnValues = [300]
+
                 const res = Utils.calculateNumber("SUM", 4, 6)
                 assert.equal(res, 10);
 
         })
         it("assert called", function () {
 
-                sendPaymentRequestToApi.returnValues = [300]
+
                 const res = Utils.calculateNumber("SUM", 4, 6)
                 assert.equal(res, 10);
-                sendPaymentRequestToApi.alwaysCalledWith("SUM", 4, 6)
+                assert(sendPaymentRequestToApi.assert.calledOnce)
         })
 })
 
