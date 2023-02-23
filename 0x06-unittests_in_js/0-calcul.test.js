1a92495378ae7c79b710d507a1f2e224c7b8135f
GetacherD <getacher.d@gmail.com>
2023-02-23 09:44:32 +0300

    round result

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,7 +4,7 @@ const assert = require("assert")
 
 it("calculateNumber", function () {
         assert.equal(calculateNumber(4, 5), 9)
-        assert.equal(calculateNumber("l", 5), "l5")
-        assert.equal(calculateNumber(1, 3.7), 4.7)
-        assert.equal(calculateNumber(1.5, 3.7), 5.2)
+
+        assert.equal(calculateNumber(1, 3.7), 5)
+        assert.equal(calculateNumber(1.5, 3.7), 6)
 })
