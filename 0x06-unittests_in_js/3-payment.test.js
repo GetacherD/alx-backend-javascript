abd47efc4d4018356e50c1d8d01532fb68aee473
GetacherD <getacher.d@gmail.com>
2023-02-23 12:00:33 +0300

    test called with

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -3,9 +3,11 @@ const Utils = require("./utils")
 const assert = require("assert")
 describe("Spy", function () {
         it("spy", function () {
-                const sendPaymentRequestToApi = spyOn(Utils, "calculateNumber").and.returnValue(300)
+                const sendPaymentRequestToApi = sinon.spy(Utils, "calculateNumber");
+                sendPaymentRequestToApi.returnValues = [300]
                 const res = Utils.calculateNumber("SUM", 4, 6)
-                assert.equal(res, 300);
+                assert.equal(res, 10);
+                sendPaymentRequestToApi.alwaysCalledWith("SUM", 4, 6)
         })
 })
 
