64a5184fe4563bb3dabdc69f9a9dec6981d92fac
GetacherD <getacher.d@gmail.com>
2023-02-25 13:41:17 +0300

    async express server

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,77 @@
+const express = require('express');
+// const fork = require('child_process');
+const fs = require('fs');
+
+const app = express();
+const fp = process.argv[2];
+console.log(fp);
+const countSync = (filePath) => new Promise((success, failure) => {
+  try {
+    const data = fs.readFileSync(filePath, 'utf-8');
+    const nd = data.split('\n').slice(1);
+    let total = 0;
+    const st = new Set();
+    const mp = new Map();
+    const length = nd[0].split(',').length - 1;
+
+    nd.forEach((d) => {
+      if (d) {
+        const va = d.split(',');
+        st.add(va[length]);
+        total += 1;
+      }
+    });
+    let result = `This is the list of our students\nNumber of students: ${total}\n`;
+    st.forEach((v) => {
+      mp.set(v, 0);
+    });
+
+    nd.forEach((d) => {
+      if (d) {
+        total += 1;
+        const key = d.split(',')[length];
+        mp.set(key, mp.get(key) + 1);
+      }
+    });
+
+    let first = 1;
+    mp.forEach((val, key) => {
+      result += `Number of students in ${key}: ${val}. List: `;
+      nd.forEach((d) => {
+        if (d) {
+          const va = d.split(',');
+          if (va[length] === key) {
+            if (first) {
+              result += ` ${va[0]}`;
+              first = 0;
+            } else {
+              result += `, ${va[0]}`;
+            }
+          }
+        }
+      });
+      first = 1;
+      result += '\n';
+    });
+    success(result.slice(0, result.length - 1));
+  } catch (e) {
+    failure(new Error('Cannot load the database'));
+  }
+});
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+app.get('/students', async (req, res) => {
+  // const ch = fork("./child.js")
+  // ch.send(process.argv[2])
+  // console.log(ch)
+  try {
+    const data = await countSync(fp);
+    res.write(data);
+    res.end();
+  } catch (e) {
+    res.end('Cannot load the database');
+  }
+});
+app.listen(1245);
+module.exports = app;
